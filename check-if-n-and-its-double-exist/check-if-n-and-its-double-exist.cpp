55f31a751559dd4a7970b230a9adfc355abfa884
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-06-25 13:07:50 +0800

    Time: 8 ms, Memory: 10.4 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -3,22 +3,18 @@ public:
     bool checkIfExist(vector<int>& arr) {
         
         // Hash map : O(N)
-        unordered_multiset<int> s;
-        for(auto e : arr)   s.insert(e);
-                
+        unordered_set<int> s;
+        for(auto e : arr)
+        {
+            if(s.find(e * 2) != s.end()) return true;
+            s.insert(e);
+        }
+        
         for(int i = 0; i < arr.size(); ++i)
         {
-            if(arr[i] % 2 == 0)
+            if(arr[i] % 2 == 0 && arr[i] != 0)
             {
-                int key = arr[i] / 2;
-                if(key == 0) 
-                {
-                    if(s.count(key) > 1) return true;
-                }
-                else
-                {
-                    if(s.find(key) != s.end()) return true;   
-                }                    
+                if(s.find(arr[i] / 2) != s.end()) return true;                     
             }
         }
         
