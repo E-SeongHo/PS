d79722b018250585aba525b8194e15d5affcfff1
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-06-25 12:53:14 +0800

    Time: 13 ms, Memory: 11.3 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         
-        // Binary search : O(logN)
-        sort(arr.begin(), arr.end());
-        
-        for(int i = arr.size() - 1; i >= 0; --i) 
+        // Hash map : O(N)
+        unordered_multiset<int> s;
+        for(auto e : arr)   s.insert(e);
+                
+        for(int i = 0; i < arr.size(); ++i)
         {
             if(arr[i] % 2 == 0)
             {
                 int key = arr[i] / 2;
-                int begin = 0;
-                int end = arr.size() - 1;
-                while(begin <= end)
+                if(key == 0) 
                 {
-                    int mid = (begin + end) / 2;
-                    if(arr[mid] == key && mid != i) return true;
-                    else if(key < arr[mid]) end = mid - 1;
-                    else begin = mid + 1;
+                    if(s.count(key) > 1) return true;
                 }
+                else
+                {
+                    if(s.find(key) != s.end()) return true;   
+                }                    
             }
         }
         
