b8a92ca15bec132215f6f37729d78a90216c5417
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-07-07 15:19:48 +0800

    Time: 3 ms, Memory: 8 MB - LeetHub

--- height-checker/height-checker.cpp
+++ height-checker/height-checker.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expected = heights;
-        sort(expected.begin(), expected.end());
         
+        // Solution2)
+        // using the constraint : 1 <= heights[i] <= 100
+        vector<int> counts(101, 0);
+        for(auto h : heights)
+            counts[h]++;
+       
+        int current = 1;
         int cnt = 0;
-        for(int i = 0; i < expected.size(); ++i)
+        
+        for(int i = 0; i < heights.size(); ++i)
         {
-            if(expected[i] != heights[i]) cnt++;
+            if(counts[current] > 0)
+            {
+                if(heights[i] != current) cnt++;
+            }
+            else
+            {
+                current++;
+                i--;
+                continue;
+            }
+            counts[current]--;
         }
         
-        return cnt;
+        return cnt;        
+        
     }
 };
