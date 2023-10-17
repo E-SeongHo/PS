3519644481fda47057a022e08b3f63e43fdc1fcd
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-10-17 19:42:36 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 84 ms, Memory: 2164 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> v;
+    string s;
+    cin >> s;
+
+    v.resize(26, vector<int>(s.size()+1));
+    
+    for(int i = 0; i < s.size(); ++i)
+    {
+        for(int j = 0; j < 26; ++j)
+        {
+            v[j][i+1] = v[j][i];
+        }
+        v[s[i]-'a'][i+1]++;
+    }
+
+    int q;
+    cin >> q;
+
+    for(int i = 0; i < q; ++i)
+    {
+        char c; 
+        int start, end;
+        cin >> c >> start >> end;
+        
+        cout << v[c-'a'][end+1] - v[c-'a'][start] << "\n";
+    }
+
+    return 0;
+}
