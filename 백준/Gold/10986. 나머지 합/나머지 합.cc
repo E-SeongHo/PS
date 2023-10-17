f88bec6bb10aa6151aa1e02e70c0bec7973c329f
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-10-17 22:32:13 +0900

    [Gold III] Title: 나머지 합, Time: 140 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    vector<long long> v;
+    v.resize(M);
+    
+    long long sum = 0;
+    for(int i = 0; i < N; ++i)
+    {
+        int x;
+        cin >> x;
+        x %= M;
+        sum += x;
+        v[sum%M]++;
+    }
+
+    long long ret = 0;
+    ret += v[0] * (v[0] + 1) * 0.5;
+    for(int i = 1; i < M; ++i)
+    {
+        ret+= (v[i] * (v[i] + 1) * 0.5) - v[i];
+    }
+
+    cout << ret;
+
+}
