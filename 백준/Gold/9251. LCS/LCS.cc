519a3f7f2c0f71d355d68050923dee8095b5f14e
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-11-11 16:49:26 +0900

    [Gold V] Title: LCS, Time: 8 ms, Memory: 5984 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -9,7 +9,7 @@ int main()
     string s1, s2;
     cin >> s1 >> s2;
 
-    if(s2.length() > s1.length()) swap(s1, s2);
+    //if(s2.length() > s1.length()) swap(s1, s2);
     vector<vector<int>> dp(s2.size()+1, vector<int>(s1.size()+1));
 
     for(int i = 1; i < s2.size()+1; ++i)
@@ -28,4 +28,3 @@ int main()
     cout << dp[s2.size()][s1.size()];
     return 0;
 }
-
