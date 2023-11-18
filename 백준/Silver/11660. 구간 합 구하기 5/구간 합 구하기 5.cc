883135f6db4368efe6fd5bdd2f597278e8ce6fef
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2023-11-18 18:25:48 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 132 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -5,15 +5,7 @@ using namespace std;
 
 int N, M;
 vector<vector<int>> table;
-
-int GetPrefixSum(int y1, int x1, int y2, int x2)
-{
-    int sum = 0;
-    for(int i = y1; i <= y2; ++i)
-        sum += table[i][x2] - table[i][x1-1]; 
-
-    return sum;
-}
+vector<vector<int>> dp;
 
 int main()
 {
@@ -23,20 +15,15 @@ int main()
 
     cin >> N >> M;
     table.resize(N+1, vector<int>(N+1));
+    dp.resize(N+1, vector<int>(N+1));
+    
 
-    int sum = 0;
     for(int i = 1; i < N+1; ++i)
     {
-        for(int j = 0; j < N+1; ++j)
+        for(int j = 1; j < N+1; ++j)
         {
-            if(j == 0) table[i][j] = table[i-1][N];
-            else
-            {
-                int n;
-                cin >> n;
-                sum += n;
-                table[i][j] = sum;
-            } 
+            cin >> table[i][j];
+            dp[i][j] = dp[i-1][j] + dp[i][j-1] + table[i][j] - dp[i-1][j-1];
         }
     }
 
@@ -45,8 +32,8 @@ int main()
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
-        int ret = GetPrefixSum(x1, y1, x2, y2);
-        cout << ret << "\n";
+        int ret = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+        cout << ret << "\n";;
     }
     return 0;
 }
