43843bdeaf505de439670b0aa5481c56c2d00b5b
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-05-19 16:45:50 +0900

    Time: 23 ms (36.33%), Space: 27.1 MB (96.89%) - LeetHub

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-
+    
+    void naive_copy(vector<int>& nums, int k)
+    {
         vector<int> copy = nums;
         for(int i = 0; i < nums.size(); i++)
         {
@@ -9,5 +10,52 @@ public:
             
             nums[target] = copy[i];
         }
+    }
+    
+    void inplace_circular(vector<int>& nums, int k)
+    {
+        int cnt = nums.size();
+        int start = 0;
+        while(cnt)
+        {
+            int current = start;
+            int value = nums[current];
+            while(true)
+            {
+                int target = (current + k) % nums.size();
+                int temp = nums[target];
+                nums[target] = value;
+                value = temp;
+                current = target;
+                cnt--;
+
+                if(current == start) break;
+            }
+            start++;
+        }
+    }
+    
+    void inplace_flip(vector<int>& nums, int k)
+    {
+        for(int i = 0; i < nums.size()/2; i++)
+        {
+            swap(nums[i], nums[nums.size()-1 - i]);
+        }
+        
+        for(int i = 0; i < k/2; i++)
+        {
+            swap(nums[i], nums[k-1-i]);
+        }
+        
+        for(int i = k; i < (nums.size()+k)/2; i++)
+        {
+            swap(nums[i], nums[nums.size()-1 - (i-k)]);
+        }
+    }
+    
+    void rotate(vector<int>& nums, int k) {
+        
+        k = k%nums.size();
+        inplace_flip(nums, k);
     }
 };
