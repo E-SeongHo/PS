30385f157786a1d490f7c2e3f595448a26d36b15
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-05-19 18:19:58 +0900

    Time: 3 ms (90.99%), Space: 13.1 MB (54.52%) - LeetHub

--- LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,8 +16,8 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
-        
+    Node* copy_naive(Node* head)
+    {
         if(head == nullptr) return nullptr;
         
         Node* copyHead = new Node(head->val);
@@ -60,5 +60,46 @@ public:
         }
         
         return copyHead;
+    }
+    
+    Node* copy_hashMap(Node* head)
+    {
+        if(head == nullptr) return nullptr;
+
+        unordered_map<Node*, Node*> hashMap;
+        
+        Node* copyHead = new Node(head->val);
+        Node* copyCurrent = copyHead;
+        Node* current = head;
+        hashMap.insert({current, copyCurrent});
+        
+        while(current->next != nullptr)
+        {
+            Node* newNode = new Node(current->next->val);
+            copyCurrent->next = newNode;
+                        
+            copyCurrent = copyCurrent->next;
+            current = current->next;
+            
+            hashMap.insert({current, copyCurrent});
+        }
+    
+        copyCurrent = copyHead;
+        current = head;
+        
+        while(copyCurrent != nullptr)
+        {
+            copyCurrent->random = hashMap[current->random];
+            
+            copyCurrent = copyCurrent->next;
+            current = current->next;
+        }
+        
+        return copyHead;
+    }
+    
+    Node* copyRandomList(Node* head) {
+        
+        return copy_hashMap(head);        
     }
 };
