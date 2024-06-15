05e337fbaba727ab442f3ab12e489a61f3540592
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-06-15 19:23:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 40 ms, Memory: 3356 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int V, E;
+vector<tuple<int, int, int>> Edges;
+vector<int> DisjointSet;
+
+
+int find(int e)
+{
+    if(DisjointSet[e] == e) return e;
+
+    else return find(DisjointSet[e]);
+}
+
+void union_set(int s1, int s2)
+{
+    int a = find(s1);
+    int b = find(s2);
+
+    a < b ? DisjointSet[b] = a : DisjointSet[a] = b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> V >> E;
+    DisjointSet.resize(V+1);
+    for(int i = 1; i < V+1; i++)
+    {
+        DisjointSet[i] = i;
+    }
+
+    Edges.reserve(E);
+
+    for(int i = 0; i < E; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        Edges.push_back({c, a, b});
+    }
+
+    sort(Edges.begin(), Edges.end());
+
+    int mst_cost = 0;
+    for(auto [weight, a, b] : Edges)
+    {
+        if(find(a) != find(b))
+        {
+            mst_cost += weight;
+            union_set(a, b);
+        }
+    }
+
+    cout << mst_cost;
+
+    return 0;
+}
