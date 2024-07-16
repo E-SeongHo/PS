bb16ec79dc83d102ae0c1cd2f57b5f9da50c402a
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-07-17 00:11:05 +0900

    [level 3] Title: 정수 삼각형, Time: 0.44 ms, Memory: 8.31 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    
+    for(int i = 1; i < triangle.size(); i++)
+    {
+        for(int j = 0; j < triangle[i].size(); j++)
+        {
+            if(j == 0)
+            {
+                triangle[i][j] += triangle[i-1][j];
+            }
+            else if(j == triangle[i].size()-1)
+            {
+                triangle[i][j] += triangle[i-1][j-1];
+            }
+            else
+            {
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j]);                
+            }
+        }
+    }
+    
+    return *max_element(triangle.back().begin(), triangle.back().end());
+}
