fea6c38075702d5a63edf297ab3c7b01cbc18baf
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-07-21 05:08:31 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.74 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -1,84 +1,56 @@
+
+#include <algorithm>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <iostream>
-#include <algorithm>
-#include <set>
 
 using namespace std;
 
-vector<char> Foods;
-vector<vector<bool>> Orders;
-
-bool check(vector<bool>& order, string& s)
-{
-    for(char c : s)
-    {
-        if(!order[c-'A']) return false;
-    }
-    
-    return true;
-}
-
-void dfs(int next, int r, string& s, vector<pair<int, string>>& candidates, int& max_cnt)
+void dfs(string& src, int next, int r, string& s, unordered_map<string, int>& m)
 {
     if(s.size() == r)
     {
-        int cnt = 0;
-        for(int i = 0; i < Orders.size(); i++)
-        {
-            if(check(Orders[i], s)) cnt++;
-        }
-        max_cnt = max(max_cnt, cnt); 
-        if(cnt >= 2) candidates.push_back({cnt, s});
-        
+        m[s]++;
         return;
     }
     
-    for(int i = next; i < Foods.size(); i++)
+    for(int i = next; i < src.size(); i++)
     {
-        s = s + Foods[i];
-        dfs(i+1, r, s, candidates, max_cnt);
+        s.push_back(src[i]);
+        dfs(src, i+1, r, s, m);
         s.pop_back();
     }
 }
 
-vector<string> solution(vector<string> orders, vector<int> course) {
+vector<string> solution(vector<string> orders, vector<int> course) 
+{
+    // 4:40
+    vector<unordered_map<string, int>> m(course.size());
     
-    Orders.resize(orders.size(), vector<bool>(26, false));
-    
-    vector<bool> foods(26, false);
-    for(int i = 0; i < orders.size(); i++)
+    for(int i = 0; i < course.size(); i++)
     {
-        for(char c : orders[i])
+        for(string& order : orders)
         {
-            foods[c-'A'] = true;
-            Orders[i][c-'A'] = true;
+            sort(order.begin(), order.end());
+            string s;
+            dfs(order, 0, course[i], s, m[i]); 
         }
     }
     
-    for(int i = 0; i < foods.size(); i++)
+    vector<string> ans;
+    for(auto& e : m)
     {
-        if(foods[i]) Foods.push_back(char(i+'A'));
-    }
-        
-    vector<string> courses;
-    for(int r : course)
-    {
-        string s;
-        vector<pair<int, string>> candidates;
-        int max_cnt = 0;
-        dfs(0, r, s, candidates, max_cnt);
-        
-        if(max_cnt < 2) continue;
-        
-        for(auto p : candidates)
-        {
-            if(p.first == max_cnt) courses.push_back(p.second);
-        }
-        
+        int max_cnt = 1;
+        for(auto& p : e) max_cnt = max(max_cnt, p.second);
+            
+        if(max_cnt < 2) continue; 
+        for(auto& p : e) 
+            if(p.second == max_cnt) ans.push_back(p.first);
     }
     
-    sort(courses.begin(), courses.end());
+    sort(ans.begin(), ans.end());
     
-    return courses;
+    // 5:07
+    return ans;
 }
