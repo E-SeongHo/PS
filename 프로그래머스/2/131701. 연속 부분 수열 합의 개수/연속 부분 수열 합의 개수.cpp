7fb512cb088adb499a7a7214eb8b7f4eaefc7c85
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-07-21 15:08:40 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 315.86 ms, Memory: 23.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -7,8 +7,28 @@ using namespace std;
 
 set<int> sums;
 
+int solve(vector<int> elements)
+{
+    int n = elements.size();
+    
+    for(int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < i+n; j++)
+        {
+            sum += elements[j%n];
+            sums.insert(sum);
+        }
+    }
+    
+    return sums.size();
+}
+
 int solution(vector<int> elements) 
 {
+    // !
+    return solve(elements);
+    
     // 2:30
     int n = elements.size();
     for(int i = 0; i < n-1; i++)
@@ -23,6 +43,6 @@ int solution(vector<int> elements)
         for(int i = 1; i < n; i++)
             sums.insert(elements[i+r-1] - elements[i-1]);
     }
-    
+    // 3:00
     return sums.size();
 }
