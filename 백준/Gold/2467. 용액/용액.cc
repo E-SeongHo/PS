0b9d8e01fd963a11a3292ba284eb5d05c8071dc6
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-09-02 18:17:39 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -10,40 +9,36 @@ vector<int> Liquids;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr); 
+    cin.tie(nullptr);
 
     cin >> N;
     Liquids.resize(N);
+
     for(int i = 0; i < N; ++i)
         cin >> Liquids[i];
 
+    int head = 0;
+    int tail = N-1;
+
     int ans = 2000000000;
-    int l1 = Liquids[0];
-    int l2 = Liquids[N-1];
-
-    for(int i = 0; i < N; ++i)
+    int l1 = Liquids[head];
+    int l2 = Liquids[tail];
+    
+    while(head < tail)
     {
-        int begin = i+1;
-        int end = N-1;
+        int combine = Liquids[head] + Liquids[tail];
 
-        while(begin <= end)
+        if(abs(combine) < ans)
         {
-            int mid = begin + (end - begin) / 2;
-        
-            int combine = Liquids[i] + Liquids[mid];
-            if(combine >= 0) end = mid-1;
-            else begin = mid+1;
-
-            if(abs(combine) < ans)
-            {
-                ans = abs(combine);
-                l1 = Liquids[i];
-                l2 = Liquids[mid];
-            }
+            ans = abs(combine);
+            l1 = Liquids[head];
+            l2 = Liquids[tail];
         }
-    }    
+
+        if(combine >= 0) tail--;
+        else head++;
+    }
 
     cout << l1 << " " << l2;
-
     return 0;
 }
