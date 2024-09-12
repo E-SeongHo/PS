d554874d9cd8ba05825d7737b55ab1a99b846258
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-09-12 17:48:39 +0900

    [Silver II] Title: 나무 자르기, Time: 148 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,82 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int N, M;
-vector<int> trees;
-vector<long long> sum; // !!!size!!!
+vector<int> Trees;
 
-int find_height(int begin, int end, int falloff, int cut)
-{
-    for(int h = end; h >= begin; --h)
-    {   
-        cut += falloff;
-        if(cut >= M) return h;
-    }
-    return -1;
-}
-
-int binary_find(int begin, int end, int falloff, int pivot_idx)
-{
-    if(begin >= end) 
-    {
-        int cut = sum[pivot_idx] + (trees[pivot_idx] - begin) * falloff;
-        if(cut < M) return begin-1;
-        else return begin;
-    }
-    
-    int mid = (begin + end) / 2;
-    long long cut = sum[pivot_idx] + (trees[pivot_idx] - mid) * (long long)falloff; 
-
-    if(cut > M) return binary_find(mid+1, end, falloff, pivot_idx);
-    else if(cut < M) return binary_find(begin, mid-1, falloff, pivot_idx);
-    else return mid;
-}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
-    
+
     cin >> N >> M;
-    trees.resize(N+1);
-    sum.resize(N+1);
+    Trees.resize(N);
+    for(int i = 0; i < N; ++i)
+        cin >> Trees[i];
 
-    for(int i = 1; i <= N; ++i)
-    {
-        cin >> trees[i];
-    }
+    int begin = 0;
+    int end = 1000000001;
 
-    if(M == 0)
+    while(begin + 1 < end)
     {
-        cout << 0;
-        return 0;
-    }
+        int mid = (begin + end) / 2;
 
-    sort(trees.begin(), trees.end());
-    
-    int idx = -1;
-    for(int i = trees.size()-2; i >= 0; --i)
-    {
-        sum[i] = (trees[i+1] - trees[i]) * (trees.size()-i-1) + sum[i+1];
-        
-        if(sum[i] >= M) 
+        long long sum = 0;
+        for(auto height : Trees)
         {
-            idx = i;
-            break;
+            if(mid < height) sum += height - mid;
         }
+        
+        if(sum >= M) begin = mid;
+        else end = mid;
     }
-
-    int falloff = trees.size()-1 - idx;
-    int begin = trees[idx];
-    int end = trees[idx+1]-1;
-    long long cut = sum[idx+1];
-
-    //int h = find_height(begin, end, falloff, cut);
-    int h = binary_find(begin, end, falloff, idx+1);
-
-    cout << h;
+    
+    cout << begin;
     return 0;
 }
