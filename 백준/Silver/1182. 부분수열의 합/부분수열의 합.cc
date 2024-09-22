3dc251d542cf5a9bb913c320b818fea9deff60ca
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-09-22 16:29:20 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
 
 int N, S;
 vector<int> V;
-map<int, int> M;
+int cnt = 0;
+
+void dfs(int depth, int total)
+{
+    if(depth == N)
+    {
+        if(total == S) cnt++;
+        return;
+    }
+
+    dfs(depth+1, total);
+    dfs(depth+1, total + V[depth]);
+}
 
 int main()
 {
     cin >> N >> S;
-
     V.resize(N);
+
     for(int i = 0; i < N; ++i)
         cin >> V[i];
 
-    for(int i = 0; i < N; ++i)
-    {
-        map<int, int> copy(M);
-        for(auto& n : copy)
-        {
-            M[n.first + V[i]] += n.second;
-        }
+    // O(2^N)
+    dfs(0, 0);
+    if(S == 0) cnt--;
 
-        M[V[i]]++;
-    }
-
-    cout << M[S];
+    cout << cnt;
 
     return 0;
 }
