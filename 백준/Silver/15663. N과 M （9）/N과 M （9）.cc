772b09c649c613ca3277db3fe353bbdae689e089
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-09-24 03:37:04 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -5,50 +5,61 @@
 using namespace std;
 
 int N, M;
-vector<int> arr;
-vector<int> set;
-vector<bool> visited;
+vector<int> V;
 
-void select(int depth)
+void dfs(vector<int>& v, vector<int>& issued)
 {
-    if(depth == M)
+    if(v.size() == M)
     {
-        for(auto s : set)
-            cout << s << " ";
-        cout << "\n";
+        for(int n : v) cout << n << " ";
+        cout << '\n';
         return;
     }
 
-    vector<int> used; 
-    for(int i = 0; i < arr.size(); ++i)
+    for(int i = 0; i < N; ++i)
     {
-        if(!visited[i])
+        if(!issued[i])
         {
-            if( binary_search(used.begin(), used.end(), arr[i]) ) continue;
-            set[depth] = arr[i];
-            visited[i] = true;
-            used.push_back(arr[i]);
-            select(depth+1);
-            visited[i] = false;
+            bool flag = false;
+            for(int j = 0; j < i; ++j)
+            {
+                if(!issued[j] && V[j] == V[i])
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(flag) continue;
+            
+            issued[i] = true;
+            v.push_back(V[i]);
+
+            dfs(v, issued);
+
+            issued[i] = false;
+            v.pop_back();
         }
     }
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
     cin >> N >> M;
+    V.resize(N);
+
     for(int i = 0; i < N; ++i)
-    {
-        int n;
-        cin >> n;
-        arr.push_back(n);
-    }    
+        cin >> V[i];
 
-    visited.resize(N);
-
-    sort(arr.begin(), arr.end());
-    set.resize(M);
-    select(0);
+    sort(V.begin(), V.end());
+    
+    vector<int> v;
+    vector<int> issued(N+1);
+    
+    dfs(v, issued);
 
     return 0;
 }
