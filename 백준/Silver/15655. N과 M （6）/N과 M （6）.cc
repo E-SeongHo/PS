8de9cb2d8c2ffcf170003baebeb7d4c30cfe2823
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-09-23 18:04:27 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> V;
+
+void dfs(int start, vector<int>& v)
+{
+    if(v.size() == M)
+    {
+        for(int n : v) cout << n << " ";
+        cout << '\n';
+
+        return;
+    }
+
+    for(int i = start; i < N; ++i)
+    {
+        v.push_back(V[i]);
+        dfs(i+1, v);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    V.resize(N);
+
+    for(int i = 0; i < N; ++i)
+        cin >> V[i];
+
+    sort(V.begin(), V.end());
+
+    vector<int> v;
+    dfs(0, v);
+
+    // vector<int> mask(N, 1);
+    // for(int i = 0; i < M; ++i)
+    //     mask[i] = 0;
+
+    // do
+    // {
+    //     for(int i = 0; i < N; ++i)
+    //     {
+    //         if(!mask[i]) cout << V[i] << " ";
+    //     } 
+    //     cout << '\n';
+
+    // } while (next_permutation(mask.begin(), mask.end()));
+    
+
+    return 0;
+}
