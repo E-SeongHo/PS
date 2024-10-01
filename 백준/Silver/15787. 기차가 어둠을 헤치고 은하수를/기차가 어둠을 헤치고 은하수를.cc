2ffd391053d1caf5e0aba0cb65c0d2a15d38fdd1
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-10-01 16:47:56 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 16 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <bitset>
+
+using namespace std;
+
+int N, M;
+vector<unsigned int> Trains;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> N >> M;
+    Trains.resize(N+1, 0);
+
+    for(int i = 0; i < M; ++i)
+    {
+        int op, train, seat;
+        cin >> op >> train;
+
+        switch(op)
+        {
+        case 1:
+            cin >> seat;
+            Trains[train] |= 1 << (seat-1);
+            break;
+        case 2:
+            cin >> seat;
+            Trains[train] &= ~(1 << (seat-1));
+            break;
+        case 3:
+            Trains[train] = Trains[train] << 1;
+            break;
+        case 4:
+            Trains[train] = Trains[train] >> 1;
+            break;
+        }
+
+        Trains[train] = Trains[train] & 0xFFFFF;
+    }
+
+    set<unsigned int> s;
+    int ans = 0;
+    for(int i = 1; i < N+1; ++i)
+    {
+        if(s.find(Trains[i]) == s.end())
+        {
+            ++ans;
+            s.insert(Trains[i]);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
