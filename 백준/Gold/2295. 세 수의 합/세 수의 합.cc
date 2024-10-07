02a72b2ff10f9dd5c9123df1f2fb3024050765ec
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-10-07 16:06:14 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,41 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
 int N;
 vector<int> V;
-set<int> S;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr); cout.tie(nullptr);
 
     cin >> N;
     V.resize(N);
     for(int i = 0; i < N; ++i)
-        cin >> V[i];
+        cin >> V[i];    
 
     sort(V.begin(), V.end());
 
+    // O(N^2)
+    vector<int> ts;
+    ts.reserve(N*N);
     for(int i = 0; i < N; ++i)
-    {
-        for(int j = 0; j < N; ++j)
-        {
-            S.insert(V[i]+V[j]);
-        }
-    }
+        for(int j = i; j < N; ++j)
+            ts.push_back(V[i]+V[j]);
 
+    sort(ts.begin(), ts.end());
+    
     for(int i = N-1; i >= 0; --i)
     {
-        for(int j = 0; j < i; ++j)
+        for(int j = i; j >= 0; --j)
         {
-            if(S.find(V[i]-V[j]) != S.end())
+            int key = V[i] - V[j];
+            if(binary_search(ts.begin(), ts.end(), key))
             {
-                cout << V[i] << endl;
+                cout << V[i];
                 return 0;
             }
         }
