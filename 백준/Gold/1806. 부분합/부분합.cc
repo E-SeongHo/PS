9dbd10475508b33799512ff7c46a857a88136c06
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-10-09 17:59:01 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-
-#define SENTINAL 9999999
+#include <algorithm>
 
 using namespace std;
 
@@ -11,38 +10,33 @@ vector<int> V;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie(nullptr); cout.tie(nullptr);
+
     cin >> N >> S;
     V.resize(N);
-
-    for(int i = 0; i < N; i++)
-    {
+    for(int i = 0; i < N; ++i)
         cin >> V[i];
-    }
 
-    int ptr = 0;
-    int qtr = -1;
-    int sum = 0;
-    int len = SENTINAL;
-
-    while(qtr < N)
+    int l = 0;
+    int r = 0;
+    int sum = V[0];
+    int ret = 99999999;
+    while(r < N && l <= r)
     {
-        if(sum < S)
+        if(sum < S) 
         {
-            // sum += V[++qtr] // array index 
-            if(++qtr >= N) break;
-
-            sum += V[qtr];
+            if(r == N-1) break;
+            r++;
+            sum += V[r];
         }
-        else
+        else 
         {
-            len = min(len, qtr - ptr + 1);
-            sum -= V[ptr++];
+            ret = min(ret, r-l+1);
+            l++;
+            sum -= V[l-1];
         }
     }
 
-    len == SENTINAL ? cout << 0 : cout << len;
-
+    ret == 99999999 ? cout << 0 : cout << ret;
     return 0;
 }
