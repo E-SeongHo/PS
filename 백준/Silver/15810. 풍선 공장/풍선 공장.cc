f9684c389467deb207b25646878c63df9c62eb14
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-10-16 17:02:25 +0900

    [Silver II] Title: 풍선 공장, Time: 416 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> Time;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cin >> N >> M;
+    Time.resize(N);
+    for(int i = 0; i < N; ++i)
+        cin >> Time[i];
+
+    long long l = 0;
+    long long r = 1000000000000ll;
+
+    while(l + 1 < r)
+    {
+        long long mid = (l+r) / 2;
+
+        long long cnt = 0;
+        for(int time : Time)
+        {
+            cnt += (mid / time);
+        }
+
+        if(cnt >= M) r = mid;
+        else l = mid;
+    }
+
+    cout << r;
+
+    return 0;
+}
