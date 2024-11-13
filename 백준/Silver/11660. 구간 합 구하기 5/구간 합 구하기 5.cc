083538523c33eb89a20cb1b566ac6eb7658048e7
Hoy Lee <76603078+E-SeongHo@users.noreply.github.com>
2024-11-13 22:15:57 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6268 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -4,36 +4,33 @@
 using namespace std;
 
 int N, M;
-vector<vector<int>> table;
-vector<vector<int>> dp;
+vector<vector<int>> Sum;
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie(nullptr); cout.tie(nullptr);
 
     cin >> N >> M;
-    table.resize(N+1, vector<int>(N+1));
-    dp.resize(N+1, vector<int>(N+1));
-    
+    Sum.resize(N+1, vector<int>(N+1, 0));
 
-    for(int i = 1; i < N+1; ++i)
+    for(int i = 1; i <= N; i++)
     {
-        for(int j = 1; j < N+1; ++j)
+        for(int j = 1; j <= N; j++)
         {
-            cin >> table[i][j];
-            dp[i][j] = dp[i-1][j] + dp[i][j-1] + table[i][j] - dp[i-1][j-1];
+            int n;
+            cin >> n;
+            Sum[i][j] = Sum[i-1][j] + Sum[i][j-1] - Sum[i-1][j-1] + n;
         }
     }
 
-    for(int i = 0; i < M; ++i)
+    for(int i = 0; i < M; i++)
     {
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
-        int ret = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
-        cout << ret << "\n";;
+        cout << Sum[x2][y2] - Sum[x2][y1-1] - Sum[x1-1][y2] + Sum[x1-1][y1-1] << '\n';
     }
+
     return 0;
 }
